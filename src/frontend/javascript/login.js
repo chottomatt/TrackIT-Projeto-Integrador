b65feccd63567ba1a819c6754e193d070cff3b7d
chottomatt <matheusrocha0110@gmail.com>
2026-09-19 18:13:56 -0300

    Toggle Password funcionando

---
+++ src/frontend/javascript/login.js
@@ -0,0 +1,16 @@
+const inputPassword = document.querySelector("#password");
+const btnEye = document.querySelector("#toggle-password");
+const eyeIcon = btnEye.querySelector("img");
+
+btnEye.addEventListener("click", function () {
+
+    if (inputPassword.type === "password") {
+        inputPassword.type = "text";
+        btnEye.setAttribute("aria-label", "Ocultar senha");
+        eyeIcon.src = "img/svg/eye-password-not-view.svg";
+    } else {
+        inputPassword.type = "password";
+        btnEye.setAttribute("aria-label", "Mostrar senha");
+        eyeIcon.src = "img/svg/eye-password-view.svg";
+    }
+});
